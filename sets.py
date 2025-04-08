100c02dcab065819d8775e7b3bd6f9a228400882
Nafemma <mpofuemmanuellah@gmail.com>
2025-04-08 17:43:25 +0200

    sets

---
+++ sets.py
@@ -0,0 +1,7 @@
+#sets are unordered collections of unique elements
+#they are mutable and can be changed
+#they are iterable and can be looped through
+#they can be nested and can contain other sets
+
+#creating a set
+set1 = {1, 2, 3, 4, 5}
