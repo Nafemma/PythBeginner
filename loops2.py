d173ce357fd61ce54c5161e31e5e124ef45158bc
Nafemma <mpofuemmanuellah@gmail.com>
2025-04-15 18:46:25 +0200

    for loops

---
+++ loops2.py
@@ -0,0 +1,10 @@
+#for loops
+#<----------------Example1---------------->
+for i in range(1,5): #stops at 4
+    print(i)
+print("Final value of i:", i)
+
+#<----------------Example2---------------->
+names = ["Mulungu Alinafe", "Isabelle", "Elizabeth", "Tinashe"]
+for name in names:
+    print(name)
